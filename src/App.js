c05275147d258b1e5f14439f44fe2a38cc24c82a
Levani49 <levani.pachuashvili.1@btu.edu.ge>
2023-03-18 16:16:38 +0400

    feat: project is ready

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Main from "./Main";
+import UserPage from "./UserPage";
+import Error from "./Error";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/user/:id" element={<UserPage />} />
+        <Route path="*" exact={true} element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
